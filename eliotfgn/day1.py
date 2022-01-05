ce528fe9ae155684dc654c212032bd0d9e9c8694
eliotfgn <efagnon2002@gmail.com>
2022-01-05 23:45:08 +0100

    day 3

--- eliotfgn/day1.py
+++ eliotfgn/day1.py
@@ -7,16 +7,14 @@ numbers = list(map(int, input().split()))
 occ = dict(Counter(numbers))
 occ = sorted(occ.items(), key=lambda x: x[1], reverse=True)
 occ = dict(occ)
-print(occ)
 t = int(input())
 
 for k in range(t):
     left, right = [int(i) for i in input().split()]
     s = 0
     for n in occ:
-        if occ[n] >= left:
-            if occ[n] <= right:
-                s += n*occ[n]
+        if left <= occ[n] <= right:
+            s += n*occ[n]
         else:
             break
     print(s)
