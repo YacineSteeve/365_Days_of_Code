b72776d69799265835b876ee3e3c4a7c634dd7b6
YacineSteeve <steeveboukari9@gmail.com>
2022-01-14 14:04:24 +0100

    Add counting sort and update data.

--- Yacine_B/sorting_algorithms/data.py
+++ Yacine_B/sorting_algorithms/data.py
@@ -3,6 +3,6 @@ from functions import *
 
 test_arrays = [[randint(1, 1000) for _ in range(10**i)] for i in range(5)]
 
-plot_arrays = [[randint(1, 1000) for _ in range(5*i)] for i in range(200)]
+plot_arrays = [[randint(1, 1000) for _ in range(2*i)] for i in range(500)]
 
-algos = [bubble, selection, insertion, comb, shaker, gnome, quick]
+algos = [bubble, selection, insertion, comb, shaker, gnome, quick, counting]
--- Yacine_B/sorting_algorithms/plot_time_complexity.py
+++ Yacine_B/sorting_algorithms/plot_time_complexity.py
@@ -3,7 +3,7 @@ from data import plot_arrays, algos
 from test_runtime import runtime
 
 
-for func in algos:
+for func in algos[:1]:
     x_y = [(len(array), runtime(func(array))) for array in plot_arrays]
     x, y = [t[0] for t in x_y], [t[1] for t in x_y]
     plt.plot(x, y)
