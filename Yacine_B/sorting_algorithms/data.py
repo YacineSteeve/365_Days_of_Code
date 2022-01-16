5b14815b5a2e7eb1e484de7f78db99a747fee478
YacineSteeve <steeveboukari9@gmail.com>
2022-01-16 23:52:02 +0100

    Add shell sort.

--- Yacine_B/sorting_algorithms/data.py
+++ Yacine_B/sorting_algorithms/data.py
@@ -6,4 +6,4 @@ test_arrays = [[randint(1, 1000) for _ in range(10**i)] for i in range(5)]
 plot_arrays = [[randint(1, 1000) for _ in range(2*i)] for i in range(500)]
 
 algos = [bubble, selection, insertion, comb, shaker, gnome, quick, counting,
-         merge]
+         merge, shell]
