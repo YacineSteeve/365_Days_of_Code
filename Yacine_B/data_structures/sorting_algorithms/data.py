3d11bb0220b76c6485a52f0d60405c57e7347c51
YacineSteeve <steeveboukari9@gmail.com>
2022-01-21 23:55:06 +0100

    Add timsort and other changes in runtime displaying

--- Yacine_B/data_structures/sorting_algorithms/data.py
+++ Yacine_B/data_structures/sorting_algorithms/data.py
@@ -1,9 +1,21 @@
 from random import randint
 from functions import *
 
-test_arrays = [[randint(1, 1000) for _ in range(10**i)] for i in range(5)]
+test_arrays = [[randint(1, 1000) for _ in range(10**i)] for i in range(7)]
 
 plot_arrays = [[randint(1, 1000) for _ in range(2*i)] for i in range(500)]
 
-algos = [bubble, selection, insertion, comb, shaker, gnome, quick, counting,
-         merge, shell, bucket]
+algos = [sorted, 
+         bubble_sort, 
+         selection_sort, 
+         insertion_sort, 
+         comb_sort, 
+         shaker_sort, 
+         gnome_sort, 
+         quick_sort, 
+         counting_sort,
+         merge_sort, 
+         shell_sort, 
+         bucket_sort, 
+         timsort
+]
