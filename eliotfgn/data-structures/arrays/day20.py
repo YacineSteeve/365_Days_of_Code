f3a6296a7781f54e9dc3ebd314eaff4623922e5e
eliotfgn <efagnon2002@gmail.com>
2022-01-22 23:55:26 +0100

    multiplication table with qt

--- eliotfgn/data-structures/arrays/day20.py
+++ eliotfgn/data-structures/arrays/day20.py
@@ -4,15 +4,14 @@ for _ in range(t):
     n, k = map(int, input().split())
     a = input()
     maxi = ""
-    p = -1
+    p = n
     for i in range(n):
         if maxi < a:
             maxi = a
             d = i
         elif maxi == a:
-          p = i-d
-          break
+            p = i - d
+            break
 
         a = a[1:] + a[:1]
-
-
+    print((d + (k-1)*p))
