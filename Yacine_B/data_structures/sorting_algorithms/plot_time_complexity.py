928b2a89f797d8b583b7dac1baeb18ec387f55bf
YacineSteeve <steeveboukari9@gmail.com>
2022-01-23 11:51:32 +0100

    Finalizing runtimes testing and ploting. End of the project.

--- Yacine_B/data_structures/sorting_algorithms/plot_time_complexity.py
+++ Yacine_B/data_structures/sorting_algorithms/plot_time_complexity.py
@@ -1,12 +1,35 @@
 import matplotlib.pyplot as plt
-from data import plot_arrays, algos
+from numpy import arange
+from scipy.optimize import curve_fit
+from data import plot_arrays, plot_colors, algos
 from test_runtime import runtime
 
 
-for func in algos[:1]:
-    x_y = [(len(array), runtime(func(array))) for array in plot_arrays]
-    x, y = [t[0] for t in x_y], [t[1] for t in x_y]
-    plt.plot(x, y)
+def objective(x, a, b, c, d, e, f):
+    	return (a * x) + (b * x**2) + (c * x**3) + (d * x**4) + (e* x**5) + f
 
+for i in range(len(algos)):
+    x, y = [], []
+    
+    print(f"\n{algos[i].__name__} running...")
 
+    for array in plot_arrays:
+        x.append(len(array))
+        y.append(runtime(algos[i](array)))
+    
+    # Explanations if phase 2 on https://machinelearningmastery.com/curve-fitting-with-python/
+    
+    popt, _ = curve_fit(objective, x, y)
+
+    a, b, c, d, e, f = popt
+
+    x_line = arange(min(x), max(x), 1)
+
+    y_line = objective(x_line, a, b, c, d, e, f)
+
+    plt.plot(x_line, y_line, color=plot_colors[i], label=algos[i].__name__)
+    
+    print(f"{algos[i].__name__} ploted!")
+
+plt.legend()
 plt.show()
--- Yacine_B/data_structures/sorting_algorithms/test_runtime.py
+++ Yacine_B/data_structures/sorting_algorithms/test_runtime.py
@@ -1,14 +1,37 @@
 import timeit
+from collections import Counter
+from operator import itemgetter
 from data import test_arrays, algos
 
 
 def runtime(f):
     test_code = f'''{f}'''
-    return timeit.timeit(stmt=test_code, number=1)
+    return timeit.timeit(stmt=test_code, number=10000)
+
+
+def best_sorting_algo(arr):
+    scores = []
+    
+    for _ in range(1000):
+        runtimes = {func: runtime(func(arr)) for func in algos}
+        best_time = min(runtimes.values())
+        
+        scores.append([f for f in runtimes if runtimes[f] == best_time][0])
+
+    scores = Counter(scores)
+
+    return sorted([(f, scores[f]//10) for f in scores], key=itemgetter(1), reverse=True)
 
 
 if __name__ == "__main__":
-    for array in test_arrays:
-        print(f"Size: {len(array)}\n"
-              f"Built-in Runtime: {runtime(algos[0](array))} s\n"
-              f"Custom Runtime: {runtime(algos[-1](array))} s\n")
+    
+    print("\nBest algorithm(s) for your array in average:\n")
+    
+    array = test_arrays[1] # Replace test_array[1] by the array to test.
+    
+    optims = best_sorting_algo(array)
+    
+    for t in optims:
+        print(f"¤ {t[0]} : {t[1]}%")
+    
+    print()
