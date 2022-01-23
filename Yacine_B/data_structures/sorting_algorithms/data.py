e9018dc883389526b4dee1577f658cd67163b7de
YacineSteeve <steeveboukari9@gmail.com>
2022-01-23 11:51:32 +0100

    Finalizing runtimes testing and ploting. End of the project.

--- Yacine_B/data_structures/sorting_algorithms/data.py
+++ Yacine_B/data_structures/sorting_algorithms/data.py
@@ -1,9 +1,15 @@
 from random import randint
 from functions import *
+import matplotlib.colors as mcolors
 
-test_arrays = [[randint(1, 1000) for _ in range(10**i)] for i in range(7)]
 
-plot_arrays = [[randint(1, 1000) for _ in range(2*i)] for i in range(500)]
+
+
+test_arrays = [[randint(1, 1000) for _ in range(10**i)] for i in range(6)]
+
+plot_arrays = [[randint(1, 100) for _ in range(50*i)] for i in range(1, 201)]
+
+plot_colors = ['black', 'gold', 'mediumblue'] + list(map(lambda s: s[4:], mcolors.TABLEAU_COLORS.keys()))
 
 algos = [sorted, 
          bubble_sort, 
