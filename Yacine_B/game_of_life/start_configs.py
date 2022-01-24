ff885ad5a1263d7298b781f3cffff3b1414e60bb
YacineSteeve <steeveboukari9@gmail.com>
2022-01-24 16:33:03 +0100

    First features for the game of life main program

--- Yacine_B/game_of_life/start_configs.py
+++ Yacine_B/game_of_life/start_configs.py
@@ -1,3 +1,16 @@
+import numpy as np
+
+WINDOW_WIDTH = 1400
+WINDOW_HEIGHT = 980
+
+CELL_SIZE = 7
+
+CELLS = np.zeros((140, 200))
+
+CELLS_POS = np.array([
+    [(-700 + x * CELL_SIZE, 490 - y * CELL_SIZE) for x in range(200)] 
+                      for y in range(140)])
+
 
 if __name__ == '__main__':
-    pass
+    print(CELLS_POS)
