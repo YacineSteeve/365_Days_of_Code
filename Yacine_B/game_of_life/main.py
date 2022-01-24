d760abb55b15a599b87ab64868b9f2852ba181e6
YacineSteeve <steeveboukari9@gmail.com>
2022-01-24 16:33:03 +0100

    First features for the game of life main program

--- Yacine_B/game_of_life/main.py
+++ Yacine_B/game_of_life/main.py
@@ -1,1 +1,27 @@
 import turtle
+from start_configs import *
+
+
+turtle.title("Game of Life")
+turtle.setup(WINDOW_WIDTH, WINDOW_HEIGHT)
+
+
+def draw_bow(location):
+    turtle.up()
+    turtle.goto(*location)
+    turtle.down()
+    for _ in range(3):
+        turtle.forward(CELL_SIZE)
+        turtle.right(90)
+    turtle.right(90)
+        
+
+def draw_grid():
+    for line in CELLS_POS:
+        for pos in line:
+            draw_bow(pos)
+            
+
+while True:
+    draw_grid()
+    turtle.getscreen()
