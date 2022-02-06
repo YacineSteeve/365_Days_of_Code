9f044563265112ac6092890164c191c1b288c6d0
Yacine BOUKARI <64146788+YacineSteeve@users.noreply.github.com>
2022-02-06 13:22:55 +0100

    Add multiple features

--- Yacine_B/mancala.c
+++ Yacine_B/mancala.c
@@ -1,11 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-/*
-
-The game description on https://en.wikipedia.org/wiki/Mancala .
-
-*/
+#include <time.h>
 
 
 typedef struct Joueur Joueur;
@@ -17,7 +12,7 @@ struct Joueur
 };
 
 
-void init(Joueur *A, int n)
+void init(Joueur *A, int n, int x)
 {
     printf("Nom du joueur %d : ", n);
     scanf("%60s", A->nom);
@@ -25,7 +20,31 @@ void init(Joueur *A, int n)
     int i=0;
     while(i<7)
     {
-        A->tab[i]=4;
+        A->tab[i]=x;
+        i++;
+    }
+}
+
+void init_IA(Joueur *A, Joueur *B, int x)
+{
+    B->nom[0] = 'I';
+    B->nom[1] = 'A';
+    B->score = 0;
+    int j=0;
+    while(j<7)
+    {
+        B->tab[j]=x;
+        j++;
+    }
+    printf("IA prête!\n\n");
+
+    printf("Nom du joueur : ");
+    scanf("%60s", A->nom);
+    A->score = 0;
+    int i=0;
+    while(i<7)
+    {
+        A->tab[i]=x;
         i++;
     }
 }
@@ -33,31 +52,33 @@ void init(Joueur *A, int n)
 
 void affiche(Joueur *A, Joueur *B)
 {
-    printf("|%2d",A->score);
+    printf("|%3d",A->score);
+
     int i=1;
     while(i<=6)
     {
-        printf("| %2d",A->tab[i]);
+        printf("| %2d", A->tab[i]);
         i++;
     }
-    printf("|  |  %s\n|  ",  A->nom);
+
+    printf("|   |  %s\n|   ", A->nom);
 
     int j=6;
-    while(j>0)
+    while(j>=1)
     {
-        printf("| %2d",B->tab[j]);
+        printf("| %2d", B->tab[j]);
         j--;
     }
-    printf ("|%2d|  %s",B->score, B->nom);
+    printf ("|%3d|  %s", B->score, B->nom);
 }
 
 
 int choix(Joueur *A)
 {
     int c;
-    
-    printf("\n\n--Tour de %s--\n", A->nom);
-    
+
+    printf("\n\n--- Tour de %s ---\n", A->nom);
+
     do
     {
         printf("Quel est votre choix (entre 1 et 6): ");
@@ -72,8 +93,85 @@ int choix(Joueur *A)
             printf("Case vide! Choisissez-en une autre.\n");
         }
     } while (c<=0 || c>6 || A->tab[c]==0);
-    
+
     return c;
+}
+
+
+int nombre_de_grains()
+{
+    int x;
+    printf("Avec combien de grains par case voulez-vous jouer? : ");
+    scanf("%d", &x);
+    return x;
+}
+
+
+int game_mode()
+{
+    int n;
+
+    printf("\n--------  Mode de jeu  --------\n");
+    printf("\n   1. J vs J");
+    printf("\n   2. J vs IA");
+    
+    do
+    {
+        printf("\n\nChoisissez un mode (entre 1 et 2) : ");
+        scanf("%d", &n);
+
+        if (n<1 || n>2)
+        {
+            printf("Non défini!");
+        }
+    } while (n<1 || n>2);
+
+    return n;
+}
+
+
+int partie()
+{
+    int n;
+
+    printf("\n--------  Type de Partie  --------\n");
+    printf("\n   1. Normal");
+    printf("\n   2. Jeu en N coups");
+    printf("\n   3. Contre-la-montre");
+    
+    do
+    {
+        printf("\n\nChoisissez une partie (entre 1 et 3) : ");
+        scanf("%d", &n);
+
+        if (n<1 || n>3)
+        {
+            printf("Non définie!");
+        }
+    } while (n<1 || n>3);
+
+    return n;
+}
+
+
+int nb_aleatoire(Joueur *B)
+{
+    int n;
+    srandom(time(NULL));
+    do
+    {    
+        n = random()%6 + 1;
+    } while (B->tab[n] == 0);
+
+    return n;
+}
+
+
+void attendre(int temps)
+{
+    clock_t arrivee = clock() + (temps*CLOCKS_PER_SEC);
+
+    while(clock() < arrivee);
 }
 
 
@@ -86,17 +184,17 @@ int full_zero(Joueur *A)
         if (A->tab[i] == 0)
         {
             n++;
-        }       
+        }
         i++;
     }
-    
+
     if (n == 6)
     {
-        return 1; // True
+        return 1;
     }
     else
     {
-        return 0; //False
+        return 0;
     }
 }
 
@@ -105,11 +203,11 @@ int arret(Joueur *A, Joueur *B)
 {
     if ((full_zero(A)==1) || (full_zero(B)==1))
     {
-        return 1; // au moins l'un des deux est en full zeros.
+        return 1;
     }
     else
     {
-        return 0; // aucun des deux n'est en full zeros.
+        return 0;
     }
 }
 
@@ -118,7 +216,7 @@ int move(Joueur *j1, Joueur *j2, int c)
 {
 
     int Main, cote; // cote=1 pour j1, 2 pour j2 et 0 pour le grenier
-    
+
     Main = j1->tab[c];
     j1->tab[c] = 0;
 
@@ -134,12 +232,9 @@ int move(Joueur *j1, Joueur *j2, int c)
             c--;
         }
 
-        c++;
-        
         if (Main > 0)
         {
             cote = 0;
-
             j1->score++;
             Main--;
         }
@@ -157,19 +252,9 @@ int move(Joueur *j1, Joueur *j2, int c)
             }
         }
 
-        if (Main > 0)
-        {
-            cote = 0;
-            j2->score++;
-            Main--;
-            
-            c = 7;
-        }
+        c = 7;
 
     }
-
-    // c contient l'indice de la dernière case visitée (côté joueur) 
-    // 7-c sera donc l'indice de la case adverse en face.
 
     if (cote == 1 && j1->tab[c] == 1)
     {
@@ -185,21 +270,49 @@ int move(Joueur *j1, Joueur *j2, int c)
 int main()
 {
     Joueur A, B;
-    int tourDeQui=1, game=0, choice, dans_grenier;
+    int tourDeQui=1, nb, mode, party, game=0, coups=1, coups_max=1000, choice, dans_grenier;
+
+    printf("\n*******************************************");
+    printf("\n*************** MANCALA 2.0 ***************");
+    printf("\n*******************************************");
+    printf("\n\n");
+
+    mode = game_mode();
+    printf("\n");
+    nb = nombre_de_grains();
+    party = partie();
+
+    if (party == 2)
+    {
+        printf("\nEn combien de coups (par joueur) voulez-vous jouer? : ");
+        scanf("%d", &coups_max);
+    }
 
     printf("\n");
 
-    init(&A, 1);
-    printf("Vous comptez de 1 à 6 de la gauche vers la droite.\n\n");
+    if (mode == 1)
+    {
+        init(&A, 1, nb);
+        printf("Vous comptez de 1 à 6 de la gauche vers la droite.\n\n");
 
-    init(&B, 2);
-    printf("Vous comptez de 1 à 6 de la droite vers la gauche.\n\n");
+        init(&B, 2, nb);
+        printf("Vous comptez de 1 à 6 de la droite vers la gauche.\n\n\n");
+    }
+    else if (mode == 2)
+    {
+        init_IA(&A, &B, nb);
+        printf("Vous comptez de 1 à 6 de la droite vers la gauche.\n\n\n");
+    }
     
+    printf("       ********************       ");
+    printf("\n*******    C'est parti!    *******\n");
+    printf("       ********************       \n\n\n");
+
     affiche(&A,&B);
-    
+
     printf("\n");
 
-    while (game==0)
+    while (game==0 && coups<=(2*coups_max))
     {
         if (tourDeQui == 1)
         {
@@ -208,17 +321,28 @@ int main()
         }
         else
         {
-            choice = choix(&B);
+            if (mode == 1)
+            {
+                choice = choix(&B);
+            }
+            else if (mode == 2)
+            {
+                printf("\n\n--- Tour de l'IA ---\n");
+                attendre(4);
+                choice = nb_aleatoire(&B);
+                printf("Je choisis la case %d\n", choice);
+            }
             dans_grenier = move(&B, &A, choice);
         }
 
         printf("\n");
 
+        attendre(1);
         affiche(&A, &B);
 
         game = arret(&A, &B);
 
-        if (dans_grenier != 0)
+        if (dans_grenier != 0 || party==2)
         {
             if (tourDeQui == 1)
             {
@@ -229,10 +353,25 @@ int main()
                 tourDeQui = 1;
             }
         }
-        
+
+        coups += 1;
     }
 
-    printf("\n\n**  Fin de la partie!  **\n\n");
+    printf("\n\n\n       *********************       ");
+    printf("\n*******  Fin de la partie!  *******\n");
+    printf("       *********************       \n\n\n");
 
-    return 0;
+    int i=1;
+    while(i<7)
+    {
+        A.score += B.tab[i];
+        B.score += A.tab[i];
+        A.tab[i] = 0;
+        B.tab[i] = 0;
+        i++;
+    }
+
+    affiche(&A, &B);
+
+    return EXIT_SUCCESS;
 }
