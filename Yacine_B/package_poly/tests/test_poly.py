0c8d0e775840b46f9a24e5884e24ea2d008efcc6
YacineSteeve <steeveboukari9@gmail.com>
2022-02-11 23:04:13 +0100

    Add unittests (coverage: 30%). Fix is_null() bug for multi-zeros coefs. Handle IndexError when init empty coefs polynomial. Refactor test_root to check_root. Refactor test_other to _check_other.

--- Yacine_B/package_poly/tests/test_poly.py
+++ Yacine_B/package_poly/tests/test_poly.py
@@ -9,58 +9,81 @@ from src.poly import *
 del sys.path[0]
 
 
+p = Poly(-5, 0, 4, -2, 1+2j, 7)
+q = Poly([5, 3, 1, -1])
+r = Poly([-5, 0, 4, -2, 1+2j, 7])
+s = Poly(-3+4j)
+t = Poly([0])
+k = Poly([-2, -7, 9])
 
 
-
-def test_init_with_string():
-    with pytest.raises(TypeError):
-        q = Poly([5, 3, '4', -1])
+def test_init():
+    with pytest.raises(IndexError):
+        a = Poly()
         
-def test_init_with_tuple():
     with pytest.raises(TypeError):
-        q = Poly([5, (3, 0), 4, -1])
+        a = Poly([5, 3, '4', -1])
+    
+    with pytest.raises(TypeError):
+        a = Poly([5, (3, 0), 4, -1])
         
-def test_init_with_list():
     with pytest.raises(TypeError):
-        q = Poly([[5, 19], 3, 4, -1])
+        a = Poly([[5, 19], 3, 4, -1])
         
-def test_init_with_bool():
     with pytest.raises(TypeError):
-        q = Poly([5, True, 4, -1])
+        a = Poly([5, True, 4, -1])
+        
         
 
 def test_is_const():
-    p = Poly(-5, 0, 4, -2, 1+2j, 7)
-    q = Poly(-3+4j)
     assert p.is_const() == False
-    assert q.is_const() == True
+    assert s.is_const() == True
     
     
 def test_is_null():
-    p = Poly([-5, 0, 4, -2, 1+2j, 7])
-    q = Poly([0])
     assert p.is_null() == False
-    assert q.is_null() == True
+    assert t.is_null() == True
 
 
-def test_get_coefs():        
-    p = Poly(-5, 0, 4, -2, 1+2j, 7)
+def test_get_coefs():
     assert p.get_coefs() == [-5, 0, 4, -2, 1+2j, 7]
     
     
 def test_evaluate():
-    q = Poly([5, 3, 4, -1])
-    assert q.evaluate(-1) == -7
-
+    assert q.evaluate(-1) == -4
+    
+    with pytest.raises(TypeError):
+        q.evaluate('string')
 
 def test_getitem():
-    q = Poly([5, 3, 4, -1])
     assert q[2] ==  3
     
     
 def test_eq():
-    p = Poly(-5, 0, 4, -2, 1+2j, 7)
-    q = Poly([5, 3, 4, -1])
-    r = Poly([-5, 0, 4, -2, 1+2j, 7])
     assert (p == q) == False
     assert (p == r) == True
+    
+    
+def test_add():
+    assert p + q == Poly(-5, 0, 9, 1, 2+2j, 6)
+    
+    
+def test_sub():
+    assert (p - r).is_null()
+    assert q - t == q
+    assert q - k == Poly(5, 5, 8, -10)
+    
+    
+def test_check_other():
+    p._check_other(Poly(4))
+    
+    with pytest.raises(TypeError):
+        p._check_other(4)
+    
+    
+def test_check_root():
+    assert k.check_root(1) == True
+    assert q.check_root(7) == False
+    
+    with pytest.raises(TypeError):
+        q.check_root('string')
