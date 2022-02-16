9e3488bc8129c0f5fd875379aacb894afbee59db
YacineSteeve <steeveboukari9@gmail.com>
2022-02-17 00:09:37 +0100

    Add functions for image zoom and rotation. Add feature scaling application with z-score normalisation.

--- Yacine_B/machine_learning/numpy_img_manip.py
+++ Yacine_B/machine_learning/numpy_img_manip.py
@@ -1,17 +1,22 @@
-from scipy import misc
 import matplotlib.pyplot as plt
+import matplotlib.image as plt_img
 import numpy as np
 
 
-face = misc.face()
-plt.imshow(face)
-plt.show()
+img = plt_img.imread('nature.jpg')
 
-length, width, _ = face.shape
 
-zoomed_face = face[length//8:7*length//8, width//8:7*width//8, :]
-plt.imshow(zoomed_face)
-plt.show()
+def zoom(picture: np.ndarray, rate: float = None):
+    if rate is None:
+        rate = 0.50
+    else:
+        if type(rate) is not float or rate >= 1.0:
+            raise ValueError("The zoom rate must be an integer smaller than 1.")
+
+    height, width, _ = picture.shape
+    reduced_h = int(height * rate / 2)
+    reduced_w = int(width * rate / 2)
+    return picture[reduced_h:height-reduced_h, reduced_w:width-reduced_w, :]
 
 
 def negative(picture: np.ndarray):
@@ -21,16 +26,30 @@ def negative(picture: np.ndarray):
 def black_white(picture: np.ndarray):
     treated = picture
     average_luminosity = picture.mean()
-    print(treated.flags)
 
-    treated[treated < average_luminosity] = 0
-    treated[treated >= average_luminosity] = 255
+    for i in range(treated.shape[2]):
+        treated[treated[:, :, i] < average_luminosity] = 0
+        treated[treated[:, :, i] >= average_luminosity] = 255
 
     return treated
 
 
-plt.imshow(negative(zoomed_face))
-plt.show()
+def rotation(picture: np.ndarray, n: int = None):
+    if n is None:
+        n = 1
+    else:
+        if type(n) is not int:
+            raise ValueError("Number of 90° rotations must be integer.")
+        
+    return np.rot90(picture, k=n)
 
-plt.imshow(black_white(zoomed_face))
+
+_, subs = plt.subplots(2, 3)
+
+subs[0, 0].imshow(img)
+subs[0, 1].imshow(zoom(img))
+subs[0, 2].imshow(rotation(img, 2))
+subs[1, 0].imshow(negative(img))
+subs[1, 1].imshow(black_white(img))
+
 plt.show()
