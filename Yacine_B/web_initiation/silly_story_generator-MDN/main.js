1c022de23d88c043caf9246691a657d3bf2b5516
YacineSteeve <steeveboukari9@gmail.com>
2022-03-21 13:31:11 +0100

    Add the html and the css to the page.

---
+++ Yacine_B/web_initiation/silly_story_generator-MDN/main.js
@@ -0,0 +1,4 @@
+let nameInput = document.querySelector(".name-input");
+let unitSystem = document.getElementsByTagName("unit");
+let genButton = document.querySelector(".generator-button");
+let storyPara = document.querySelector(".story-para");
