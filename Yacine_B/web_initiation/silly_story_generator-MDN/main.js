fb5902c0f16398e1241d7a994a9f60d01fddb16d
YacineSteeve <steeveboukari9@gmail.com>
2022-03-22 17:53:46 +0100

    Add a function to select random string for each gap. Add a function to replace the gaps by the random strings selected. Add a function to display the new story made with the random strings.

--- Yacine_B/web_initiation/silly_story_generator-MDN/main.js
+++ Yacine_B/web_initiation/silly_story_generator-MDN/main.js
@@ -1,4 +1,81 @@
-let nameInput = document.querySelector(".name-input");
-let unitSystem = document.getElementsByTagName("unit");
-let genButton = document.querySelector(".generator-button");
+const customNameInput = document.querySelector(".name-input");
+const unitSystemRadio = document.querySelectorAll(".unit-radio");
+const genButton = document.querySelector(".generator-button");
+
 let storyPara = document.querySelector(".story-para");
+
+const rawText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
+
+const stringX = ["Willy the Goblin",
+               "Big Daddy",
+               "Father Christmas"];
+
+const stringY = ["the soup kitchen",
+               "Disneyland",
+               "the White House"];
+
+const stringZ = ["spontaneously combusted",
+               "melted into a puddle on the sidewalk",
+               "turned into a slug and crawled away"];
+
+const strings = [stringX, stringY, stringZ];
+
+
+customNameInput.focus();
+storyPara.style.visibility = "hidden";
+genButton.addEventListener("click", displayStory);
+
+
+function selectRandomStrings() {
+    let randomStrings = [];
+
+    for (let i = 0; i < strings.length; i++) {
+        let index = Math.floor(Math.random() * strings[i].length);
+        randomStrings.push(strings[i][index]);
+    }  
+
+    return randomStrings;
+}
+
+
+function replaceStrings(storyText) {
+    let toReplace = [":insertx:", ":inserty:", ":insertz:"];
+    let newStrings = selectRandomStrings();
+    let newStoryText = storyText;
+
+    for (let i = 0; i < toReplace.length; i++) {
+        if (i === 0) {
+            newStoryText = newStoryText.replace(toReplace[i], newStrings[i]);
+        }
+        newStoryText = newStoryText.replace(toReplace[i], newStrings[i]);
+    }
+
+    return newStoryText;
+}
+
+
+function displayStory() {
+    let newStory = rawText;
+    let customName = customNameInput.value;
+    let unitSystem;
+
+    for (const radio of unitSystemRadio) {
+        if (radio.checked) {
+            unitSystem = radio.value;
+        }
+    }
+
+    if (customName !== "") {
+        newStory = newStory.replace("Bob", customName);
+    }
+
+    newStory = replaceStrings(newStory);
+
+    storyPara.textContent = newStory;
+
+    if (storyPara.style.visibility = "hidden") {
+        storyPara.style.visibility = "visible";
+    }
+
+    customNameInput.focus();
+}
