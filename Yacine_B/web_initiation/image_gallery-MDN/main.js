6694686f3bc24b007e03b557617edce4f9ce9cc6
YacineSteeve <steeveboukari9@gmail.com>
2022-03-28 12:03:33 +0200

    The displayed image become the image that is clicked.

--- Yacine_B/web_initiation/image_gallery-MDN/main.js
+++ Yacine_B/web_initiation/image_gallery-MDN/main.js
@@ -1,1 +1,14 @@
-// TODO
+const displayedImage = document.getElementById('displayed');
+const toDisplayImages = document.getElementsByClassName('to-display-img');
+
+function getUrl(div) {
+    return window.getComputedStyle(div).backgroundImage;
+}
+
+function display(e) {
+    displayedImage.style.backgroundImage = getUrl(e.target);
+}
+
+for (let i = 0; i < toDisplayImages.length; i++) {
+    toDisplayImages[i].addEventListener('click', display);
+}
