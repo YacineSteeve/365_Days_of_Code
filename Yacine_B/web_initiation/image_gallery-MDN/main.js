582d46dbfa61cdedb4129110aba440afb9e25840
YacineSteeve <steeveboukari9@gmail.com>
2022-03-31 00:07:07 +0200

    Last commit. Add the suitable function for click event.

--- Yacine_B/web_initiation/image_gallery-MDN/main.js
+++ Yacine_B/web_initiation/image_gallery-MDN/main.js
@@ -1,6 +1,7 @@
-const displayedImage = document.getElementById("displayed");
-const toDisplayImages = document.getElementsByClassName("to-display-img");
-const lumButton = document.getElementById("luminosity-btn");
+let displayedImage = document.getElementById("displayed");
+let toDisplayImages = document.getElementsByClassName("to-display-img");
+let lumButton = document.getElementById("luminosity-btn");
+let veilDiv = document.getElementById("veil");
 
 
 function getUrl(div) {
@@ -12,6 +13,19 @@ function display(evt) {
     // Set a new displayed image url with the one from the clicked element.
     displayedImage.style.backgroundImage = getUrl(evt.target);
 }
+
+function toggleLuminosity(evt) {
+    if (evt.target.textContent === "Darken") {
+        veil.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
+        evt.target.textContent = "Lighten";
+    }
+    else {
+        veil.style.backgroundColor = "rgba(0, 0, 0, 0.0)";
+        evt.target.textContent = "Darken";
+    }
+}
+
+lumButton.addEventListener("click", toggleLuminosity);
 
 for (let i = 0; i < toDisplayImages.length; i++) {
     toDisplayImages[i].addEventListener("click", display);
