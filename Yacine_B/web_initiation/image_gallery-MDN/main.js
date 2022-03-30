ce09086f9d0442bb1372612f4c2b84634f8e2b6e
YacineSteeve <steeveboukari9@gmail.com>
2022-03-30 23:18:49 +0200

    Add a veil and a button to the displayed image to change its luminosity.

--- Yacine_B/web_initiation/image_gallery-MDN/main.js
+++ Yacine_B/web_initiation/image_gallery-MDN/main.js
@@ -1,12 +1,16 @@
 const displayedImage = document.getElementById("displayed");
 const toDisplayImages = document.getElementsByClassName("to-display-img");
+const lumButton = document.getElementById("luminosity-btn");
+
 
 function getUrl(div) {
+    // Return the background image url for a given element.
     return window.getComputedStyle(div).backgroundImage;
 }
 
-function display(e) {
-    displayedImage.style.backgroundImage = getUrl(e.target);
+function display(evt) {
+    // Set a new displayed image url with the one from the clicked element.
+    displayedImage.style.backgroundImage = getUrl(evt.target);
 }
 
 for (let i = 0; i < toDisplayImages.length; i++) {
