0f670b1ff401ac43e92c60a723d6411f451d8224
YacineSteeve <steeveboukari9@gmail.com>
2022-04-06 11:21:12 +0200

    Solution submitted for https://open.kattis.com/problems/combinationlock

--- Yacine_B/kattis.py
+++ Yacine_B/kattis.py
@@ -1,1 +1,27 @@
-n, m = map(int, input().split())
+given = list(map(int, input().split()))
+pos, *code = given
+
+dial = [i for i in range(40)]
+
+
+def f(x):
+    return x * 360 // 40
+
+
+while any(map(lambda x: x != 0, given)):
+    deg = 3 * f(40)
+
+    for index in range(len(code)):
+        k = dial.index(pos)
+        while dial[k % len(dial)] != code[index]:
+            if index in [0, 2]:
+                k -= 1
+            else:
+                k += 1
+            deg += f(1)
+        pos = code[index]
+
+    print(deg)
+
+    given = list(map(int, input().split()))
+    pos, *code = given
