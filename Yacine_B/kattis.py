64311526efedf3ab68ada4e544e7ee00fd0bad42
YacineSteeve <steeveboukari9@gmail.com>
2022-04-07 14:24:51 +0200

    Solution submitted for https://open.kattis.com/problems/anothercandies

--- Yacine_B/kattis.py
+++ Yacine_B/kattis.py
@@ -1,27 +1,6 @@
-given = list(map(int, input().split()))
-pos, *code = given
+for _ in range(int(input())):
+    _, n = input(), int(input())
 
-dial = [i for i in range(40)]
+    candies = [int(input()) for _ in range(n)]
 
-
-def f(x):
-    return x * 360 // 40
-
-
-while any(map(lambda x: x != 0, given)):
-    deg = 3 * f(40)
-
-    for index in range(len(code)):
-        k = dial.index(pos)
-        while dial[k % len(dial)] != code[index]:
-            if index in [0, 2]:
-                k -= 1
-            else:
-                k += 1
-            deg += f(1)
-        pos = code[index]
-
-    print(deg)
-
-    given = list(map(int, input().split()))
-    pos, *code = given
+    print("YES" if not sum(candies) % n else "NO")
