57b35d04a62588287c38fd8bd706c1af21d0989f
YacineSteeve <steeveboukari9@gmail.com>
2022-04-10 23:36:00 +0200

    Create a constructor for training.

--- Yacine_B/web_initiation/opp.js
+++ Yacine_B/web_initiation/opp.js
@@ -1,16 +1,50 @@
-function Car(owner, brand, model, year, color, energy) {
-    this.brand = brand;
-    this.model = model;
-    this.year = year;
-    this.color = color;
-    this.energy = energy;
-    this.owner = {
-        ownerName: owner[0],
-        ownerSex: owner[1]
-    }
-}
+function Personne(prenom, famille, age, genre, interets) {
+    this.nom = {
+        'prenom': prenom,
+        'famille' : famille
+    };
+    this.age = age;
+    this.genre = genre;
+    this.interets = interets;
+    this.bio = function() {
+        let string = this.nom.prenom + ' ' + this.nom.famille + ' a ' + this.age + ' ans. ';
+        let pronom;
 
-let myLambo = new Car(['Yacine', 'Male'], 'Lamborghini',
-    'Aventador', '2012', 'green', 'fuel');
+        if (this.genre === 'homme' || this.gender === 'Homme' || this.gender === 'h' || this.gender === 'H') {
+            pronom = 'Il aime ';
+        }
+        else if (this.genre === 'femme' || this.gender === 'Femme' || this.gender === 'f' || this.gender === 'F') {
+            pronom = 'Elle aime ';
+        }
+        else {
+            pronom = this.nom.prenom + ' aime ';
+        }
+        string += pronom;
 
-console.log(myLambo.owner);
+        if (this.interets.length === 1) {
+            string += this.interets[0] + '.';
+        }
+        else if (this.interets.length === 2) {
+            string += this.interets[0] + ' et ' + this.interets[1] + '.';
+        }
+        else {
+            for (let i = 0; i < this.interets.length; i++) {
+                if (i === this.interets.length - 1) {
+                    string += 'et ' + this.interets[i] + '.';
+                }
+                else {
+                    string += this.interets[i] + ', ';
+                }
+            }
+        }
+
+        alert(string);
+    };
+    this.salutation = function() {
+        alert('Bonjour ! Je m\'appelle ' + this.nom.prenom + '.');
+    };
+};
+
+let personne1 = new Personne('Jean', 'Biche', 32, 'neutre', ['musique', 'tricot', 'boxe']);
+
+console.log(personne1.prototype);
