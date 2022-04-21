31d18baf75c45e47359f8086e9f8ed8be2aea90c
YacineSteeve <steeveboukari9@gmail.com>
2022-04-21 21:10:31 +0200

    An exercice for input/output with NodeJS.
    Signed-off-by: YacineSteeve <steeveboukari9@gmail.com>

---
+++ Yacine_B/web_initiation/bases.js
@@ -0,0 +1,11 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('\nEnter x: ', (answer) => {
+    console.log(`\nx = ${answer}`);
+    rl.close();
+});
