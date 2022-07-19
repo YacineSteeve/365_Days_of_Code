7d1677854508b632a683666beb2afb7fd660471b
Yacine BOUKARI <steeveboukari9@gmail.com>
2022-07-19 14:21:41 +0200

    Submission for https://open.kattis.com/problems/queens

--- Yacine_B/kattis.py
+++ Yacine_B/kattis.py
@@ -1,5 +1,33 @@
-n, p = input().split()
+from itertools import combinations
+from math import atan, pi
 
-desc = [input() for _ in range(n)]
 
-print(p)
+def arg(u, v):
+    return abs(atan((v[1] - u[1]) / (v[0] - u[0])))
+
+
+def solution():
+    n = int(input())
+    queens, rows, cols = [], [], []
+    ok = True
+
+    for _ in range(n):
+        queen = input().split()
+        if queen[0] in cols or queen[1] in rows:
+            ok = False
+        else:
+            queens.append(tuple(map(int, queen)))
+            cols.append(queen[0])
+            rows.append(queen[1])
+
+    if not ok:
+        return False
+
+    for q1, q2 in combinations(queens, 2):
+        if arg(q1, q2) == pi / 4:
+            return False
+
+    return True
+
+
+print("CORRECT") if solution() else print("INCORRECT")
