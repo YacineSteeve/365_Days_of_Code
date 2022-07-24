3c8f46a6a73fc2b00267dbced93c7e37249d212e
Yacine BOUKARI <steeveboukari9@gmail.com>
2022-07-24 13:58:09 +0200

    Submission for https://open.kattis.com/problems/nastyhacks

--- Yacine_B/kattis.py
+++ Yacine_B/kattis.py
@@ -1,3 +1,9 @@
-n = int(input())
+for _ in range(int(input())):
+    r, e, c = map(int, input().split())
 
-print(sum(map(int, input().split())))
+    if e - c > r:
+        print('advertise')
+    elif e - c < r:
+        print('do not advertise')
+    elif e - c == r:
+        print('does not matter')
