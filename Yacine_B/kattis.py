47da4b6d8005d28e3ceb0013054a0903cf5d2661
Yacine BOUKARI <steeveboukari9@gmail.com>
2022-07-28 17:17:34 +0200

    Submission for https://open.kattis.com/problems/cocktail

--- Yacine_B/kattis.py
+++ Yacine_B/kattis.py
@@ -1,1 +1,14 @@
-print(sum(map(int, input().split())))
+n, t = map(int, input().split())
+potions = []
+
+ok = True
+
+for i in range(n):
+    potions = list(map(lambda x: x - t, potions))
+    potions.append(int(input()))
+
+    if i == n - 1 and 0 in potions:
+        ok = False
+        break
+
+print("YES") if ok else print("NO")
