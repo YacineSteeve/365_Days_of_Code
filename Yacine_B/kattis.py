5e1ab105cd734a37a5568a1ec6380a0fedd23086
Yacine BOUKARI <steeveboukari9@gmail.com>
2022-08-01 18:32:54 +0200

    Submission for https://open.kattis.com/problems/filip

--- Yacine_B/kattis.py
+++ Yacine_B/kattis.py
@@ -1,12 +1,7 @@
-x, y, n = map(int, input().split())
+def swap(x):
+    return int(''.join(reversed(list(x))))
 
-for i in range(1, n + 1):
-    if i % x == 0:
-        if i % y == 0:
-            print("FizzBuzz")
-        else:
-            print("Fizz")
-    elif i % y == 0:
-        print("Buzz")
-    else:
-        print(i)
+
+a, b = map(swap, input().split())
+
+print(max(a, b))
