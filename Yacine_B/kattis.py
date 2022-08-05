cf4595286b9bbe9e60ba93d5f2a35d505308550e
Yacine BOUKARI <steeveboukari9@gmail.com>
2022-08-05 09:10:42 +0100

    Submission for https://open.kattis.com/problems/bijele

--- Yacine_B/kattis.py
+++ Yacine_B/kattis.py
@@ -1,44 +1,4 @@
-n, dom = input().split()
+norm = [1, 1, 2, 2, 2, 8]
 
-score = 0
-
-points = {
-    "A": {
-        "Dom": 11,
-        "Non_Dom": 11,
-    },
-    "K": {
-        "Dom": 4,
-        "Non_Dom": 4,
-    },
-    "Q": {
-        "Dom": 3,
-        "Non_Dom": 3,
-    },
-    "J": {
-        "Dom": 20,
-        "Non_Dom": 2,
-    },
-    "T": {
-        "Dom": 10,
-        "Non_Dom": 10,
-    },
-    "9": {
-        "Dom": 14,
-        "Non_Dom": 0,
-    },
-    "8": {
-        "Dom": 0,
-        "Non_Dom": 0,
-    },
-    "7": {
-        "Dom": 0,
-        "Non_Dom": 0,
-    },
-}
-
-for _ in range(4 * int(n)):
-    hand = input()
-    score += points[hand[0]]["Dom"] if hand[1] == dom else points[hand[0]]["Non_Dom"]
-
-print(score)
+for i, n in enumerate(list(map(int, input().split()))):
+    print(norm[i] - n, end=' ')
