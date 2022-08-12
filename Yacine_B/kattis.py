be42bf763729acb499d97bafed36d0d3c98224b2
Yacine BOUKARI <steeveboukari9@gmail.com>
2022-08-12 23:54:13 +0100

    Submission for https://open.kattis.com/problems/trik

--- Yacine_B/kattis.py
+++ Yacine_B/kattis.py
@@ -1,11 +1,27 @@
-l, d, x = map(int, (input(), input(), input()))
+game = ["ball", "empty", "empty"]
 
 
-def digit_sum(n):
-    return sum(map(int, list(str(n))))
+def move_a(cups):
+    cups[0], cups[1] = cups[1], cups[0]
 
 
-matches = [i for i in range(l, d+1) if digit_sum(i) == x]
+def move_b(cups):
+    cups[1], cups[2] = cups[2], cups[1]
 
-print(min(matches))
-print(max(matches))
+
+def move_c(cups):
+    cups[0], cups[2] = cups[2], cups[0]
+
+
+moves = {
+    "A": move_a,
+    "B": move_b,
+    "C": move_c
+}
+
+for move in input():
+    moves[move](game)
+
+for i, cup in enumerate(game):
+    if cup == "ball":
+        print(i+1)
