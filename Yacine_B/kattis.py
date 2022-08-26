ee93dfb7e8e3712148e876e7d44e6d620bb74605
Yacine BOUKARI <steeveboukari9@gmail.com>
2022-08-26 15:58:32 +0200

    Submission for https://open.kattis.com/problems/spavanac

--- Yacine_B/kattis.py
+++ Yacine_B/kattis.py
@@ -1,9 +1,2 @@
-h, m = map(int, input().split())
-
-if m >= 45:
-    print(f'{h} {m-45}')
-else:
-    if h == 0:
-        print(f'{23} {m+15}')
-    else:
-        print(f'{h-1} {m+15}')
+print(bin(11))
+print(int(bin(13)[2:][::-1]))
