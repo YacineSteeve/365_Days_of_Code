f8213c2f5502e9e131b7000c7334e2a8125196cb
Yacine BOUKARI <steeveboukari9@gmail.com>
2022-10-01 00:21:07 +0200

    Submission for https://open.kattis.com/contests/nqi2yi/problems/chartingprogress
    Signed-off-by: Yacine BOUKARI <steeveboukari9@gmail.com>

--- Yacine_B/kattis.py
+++ Yacine_B/kattis.py
@@ -1,5 +1,35 @@
-from math import sin, radians, ceil
+new = True  # Nouvelle série de logs dès au départ
+l = 0  # longueur totale initiale nulle
+dots = 0  # nombre de points à placer à la fin
 
-h, v = map(int, input().split())
+while True:
+    try:
+        curr = input().strip()  # Current line
+    except:  # Donc enf of file
+        break
 
-print(ceil(h / sin(radians(v))))
+    if new:
+        l = len(curr)  # Longueur de la ligne (reste la même pour toute la série de logs)
+
+    if curr == '':
+        new = True
+        dots = 0  # Réinitialise le nombre de points finaux
+        print()  # Ligne vide
+        continue  # Jump directement vers la prochaine ligne
+    else:
+        new = False
+
+    # Ex:
+    # ...........*........
+    # ....*.....*.........
+    # .........*..*...*...
+    # Qui devient
+    # ...................*
+    # .................**.
+    # ..............***...
+
+    asts = curr.count('*')
+
+    print('.' * (l - (asts + dots)) + '*' * asts + '.' * dots)
+
+    dots += asts  # Le nombre de points finaux = la somme des astérisques des lignes précédentes
