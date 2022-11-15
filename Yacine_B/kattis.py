a54a48167e8ccd3c085f67e0469b1c1c7189eb98
Yacine BOUKARI <steeveboukari9@gmail.com>
2022-11-15 15:23:51 +0100

    Submission for https://open.kattis.com/problems/hangman

--- Yacine_B/kattis.py
+++ Yacine_B/kattis.py
@@ -1,8 +1,16 @@
-s = input()
-res = []
+word, letters = set(input()), input()
+wrongs = 0
 
-for c in s:
-    if not res or (res and c != res[-1]):
-        res.append(c)
+for letter in letters:
+    if letter in word:
+        word.remove(letter)
+    else:
+        wrongs += 1
 
-print(''.join(res))
+    if not word:
+        print("WIN")
+        break
+
+    if wrongs == 10:
+        print("LOSE")
+        break
