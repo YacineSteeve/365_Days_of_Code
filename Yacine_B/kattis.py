f9fb8da1b695f713fbc75b3268b8dda2cc37d9bc
Yacine BOUKARI <steeveboukari9@gmail.com>
2022-11-16 12:03:08 +0100

    Submission for https://open.kattis.com/problems/apaxiaaans

--- Yacine_B/kattis.py
+++ Yacine_B/kattis.py
@@ -1,35 +1,8 @@
-new = True  # Nouvelle série de logs dès au départ
-l = 0  # longueur totale initiale nulle
-dots = 0  # nombre de points à placer à la fin
+s = input()
+res = []
 
-while True:
-    try:
-        curr = input().strip()  # Current line
-    except:  # Donc enf of file
-        break
+for c in s:
+    if not res or (res and c != res[-1]):
+        res.append(c)
 
-    if new:
-        l = len(curr)  # Longueur de la ligne (reste la même pour toute la série de logs)
-
-    if curr == '':
-        new = True
-        dots = 0  # Réinitialise le nombre de points finaux
-        print()  # Ligne vide
-        continue  # Jump directement vers la prochaine ligne
-    else:
-        new = False
-
-    # Ex:
-    # ...........*........
-    # ....*.....*.........
-    # .........*..*...*...
-    # Qui devient
-    # ...................*
-    # .................**.
-    # ..............***...
-
-    asts = curr.count('*')
-
-    print('.' * (l - (asts + dots)) + '*' * asts + '.' * dots)
-
-    dots += asts  # Le nombre de points finaux = la somme des astérisques des lignes précédentes
+print(''.join(res))
