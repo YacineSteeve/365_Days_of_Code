e894f2cd1ae6502031b7cfbdcad244a291895b29
Yacine BOUKARI <steeveboukari9@gmail.com>
2022-12-05 12:12:06 +0100

    Submission for https://open.kattis.com/problems/detaileddifferences

--- Yacine_B/kattis.py
+++ Yacine_B/kattis.py
@@ -1,2 +1,10 @@
-s = input().strip()
-print(int(len(set(s)) == len(s)))
+for _ in range(int(input())):
+    s1, s2 = input(), input()
+    print(s1)
+    print(s2)
+    for i in range(len(s1)):
+        if s1[i] == s2[i]:
+            print('.', end='')
+        else:
+            print('*', end='')
+    print()
